5e42bd71010ca820dc08665bb5c9d9d7e1ae415a
Duy Ngo <106200147+Duybo007@users.noreply.github.com>
2023-10-04 12:29:59 -0700

    start angular and connect to API

--- API/Program.cs
+++ API/Program.cs
@@ -8,13 +8,13 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
-    
+builder.Services.AddCors();
 builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
